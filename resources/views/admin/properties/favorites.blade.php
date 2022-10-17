550a1ce76699e8bebbf0f3816c02a9d823ed1b2f
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-10-17 20:30:31 +0300

    Translating Admin Panel

---
+++ resources/views/admin/properties/favorites.blade.php
@@ -0,0 +1,66 @@
+@extends('backend.layouts.app')
+
+@section('title', 'Properties')
+
+@push('styles')
+
+    <!-- JQuery DataTable Css -->
+    <link rel="stylesheet" href="{{ asset('backend/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css') }}">
+
+@endpush
+
+@section('content')
+
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header bg-indigo">
+                    <h2>قائمة المفضلين لعقار </h2>
+                </div>
+                <div class="body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>الصورة</th>
+                                    <th width="150">-</th>
+                                </tr>
+                            </thead>
+
+                            {{-- <tbody>
+                                @foreach( $properties->users() as $user )
+                                <tr>
+                                    <td>{{$user->name}}</td>
+
+                                </tr>
+                                @endforeach --}}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+@push('scripts')
+
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="{{ asset('backend/plugins/jquery-datatable/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/jszip.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/jquery-datatable/extensions/export/buttons.print.min.js') }}"></script>
+
+    <!-- Custom Js -->
+    <script src="{{ asset('backend/js/pages/tables/jquery-datatable.js') }}"></script>
+
+
+@endpush
