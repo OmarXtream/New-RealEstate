1144f7915f1cce4de0f405c3dfdb6623ccc477e4
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-10-18 17:44:59 +0300

    Open Graph Protocol

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -12,6 +12,11 @@
 <!-- Fav Icon -->
 <link rel="icon" href="{{asset('frontend/images/favicon.png')}}" type="image/x-icon">
 
+<meta property="og:title" content="روشم - Rushm" />
+<meta property="og:site_name" content="Rushm" />
+<meta property="og:type" content="realestate" />
+<meta property="og:image" content="https://k.top4top.io/p_2482vbjg61.png" />
+
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
