b7be4f4e6fa08bbadbab537471d8248c06966a3f
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-10-21 17:55:00 +0300

    Rushm content

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -3,33 +3,7 @@
             <div class="footer-top bg-color-2">
                 <div class="auto-container">
                     <div class="row clearfix">
-                        <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget about-widget">
-                                <div class="widget-title">
-                                    <h3>الرؤية</h3>
-                                </div>
-                                <div class="text">
-                                    @if(isset($footersettings[0]) && $footersettings[0]['aboutus'])
-                                    <p class="grey-text text-lighten-4">{{ $footersettings[0]['aboutus'] }}</p>
-                                    @else
-                                    <p class="grey-text text-lighten-4">رؤيتنا هي تحقيق النجاح وخلق معايير جديدة في الإستثمار العقاري من خلال مستويات عالية من الخبرة والإبداع</p>
-                                    @endif
-                                 </div>
-
-                                 <div class="widget-title mt-2">
-                                    <h3>الهدف</h3>
-                                </div>
-                                <div class="text">
-                                    @if(isset($footersettings[0]) && $footersettings[0]['aboutus'])
-                                    <p class="grey-text text-lighten-4">{{ $footersettings[0]['aboutus'] }}</p>
-                                    @else
-                                    <p class="grey-text text-lighten-4">أن نكون أحد الشركات الرائدة في عالم العقارات وأن نستمر في توسيع بصمتنا وإستراتجيتنا في النمو من أجل خلق قيمة لعملائنا</p>
-                                    @endif
-                                 </div>
-
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+                        <div class="col-lg-6 col-md-6 col-sm-12 footer-column">
                             <div class="footer-widget links-widget ml-70">
                                 <div class="widget-title">
                                     <h3>القائمة</h3>
@@ -59,7 +33,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+                        <div class="col-lg-6 col-md-6 col-sm-12 footer-column">
                             <div class="footer-widget post-widget">
                                 <div class="widget-title">
                                     <h3>العقارات الأخيرة</h3>
