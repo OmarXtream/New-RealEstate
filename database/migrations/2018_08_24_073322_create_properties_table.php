6407c6fb13c7f39a4834dabaa6b5649db0e6a4cd
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2023-03-09 22:04:52 +0300

    update property area type

--- database/migrations/2018_08_24_073322_create_properties_table.php
+++ database/migrations/2018_08_24_073322_create_properties_table.php
@@ -27,7 +27,7 @@ class CreatePropertiesTable extends Migration
             $table->string('city');
             $table->string('city_slug');
             $table->string('address');
-            $table->integer('area');
+            $table->string('area');
             $table->integer('agent_id');
             $table->text('description');
             $table->string('video')->nullable();
